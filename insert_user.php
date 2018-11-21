5ab17f30321f87043c90d2428e978685f04d5c45
mciucanu <ciucanumatei@gmail.com>
2018-11-21 15:50:46 -0800

    send userid from create new user

--- insert_user.php
+++ insert_user.php
@@ -15,7 +15,8 @@ $query = "INSERT INTO users (email, username, password) VALUES ('$email', '$user
 
 $result = $conn->query($query);
 if($result){
-  echo json_encode(true);
+  $allowed = $result->fetchAll();
+  echo json_encode($allowed);
 } else {
   echo json_encode(false);
 }
